39af8fc29b2f62cb8f6a97ef69fc364e6b4e9949
pribakil <bakengailunga@gmail.com>
2023-06-08 14:25:40 +0100

    feat: Carousel tested

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const FeaturedPostCard = ({ post }) => (
   <div className="relative h-72">
     <div
-      className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72"
+      className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-sm shadow-white inline-block w-full h-72"
       style={{ backgroundImage: `url('${post.featuredImage.url}')` }}
     />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
